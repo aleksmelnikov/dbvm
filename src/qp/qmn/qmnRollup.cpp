591483a4602291e3c39fbf82867f503b53b5e32b
melnikov <dailyadm@yandex.ru>
2026-04-26 11:30:28 +0300

    chore(qmn): mark variables as unused -- src/qp/qmn/qmnRollup.cpp
    Note:
    [ fix warnings ]
    
    src/qp/qmn/qmnRollup.cpp:905:47: warning: unused parameter 'aCodePlan' [-Wunused-parameter]

--- src/qp/qmn/qmnRollup.cpp
+++ src/qp/qmn/qmnRollup.cpp
@@ -927,6 +927,9 @@ IDE_RC qmnROLL::execAggregation( qcTemplate * aTemplate,
         else
         {
             IDE_DASSERT( i < aCodePlan->distNodeCount );
+
+            ACP_UNUSED(aCodePlan);
+
             if ( aDataPlan->distNode[aGroupIndex][i].isDistinct == ID_TRUE )
             {
                 IDE_TEST( qtc::aggregate( sNode->dstNode, aTemplate )
