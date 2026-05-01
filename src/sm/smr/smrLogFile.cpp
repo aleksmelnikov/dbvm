90cb9ad770e774505a95eecb088efa978a607572
melnikov <dailyadm@yandex.ru>
2026-05-01 09:36:26 +0300

    chore(smr): mark variables as unused -- src/sm/smr/smrLogFile.cpp
    Note:
    [ fix warnings ]
    
    src/sm/smr/smrLogFile.cpp:1404:13: warning: variable 'sum' set but not used [-Wunused-but-set-variable]

--- src/sm/smr/smrLogFile.cpp
+++ src/sm/smr/smrLogFile.cpp
@@ -1423,6 +1423,7 @@ void smrLogFile::touchMMapArea()
        아무 의미 없음.*/
     return sum;
 #endif
+    ACP_UNUSED(sum);
 }
 
 /***********************************************************************
